d0b97d14c2bde7ae127a3badb5690ac63cb59738
Ayush Raj Gupta <ayush.r.exe@gmail.com>
2026-06-05 03:20:20 +0530

    Add: add-two-numbers - submission-5

---
+++ Data Structures & Algorithms/add-two-numbers/submission-5.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carr = 0
+        # nodes = []
+        temp1 = l1
+        temp2 = l2
+        sol = ListNode(-1)
+        dummyNode = sol
+        while temp1 and temp2:
+            value : int = (temp1.val + temp2.val + carr)
+            val = value % 10 
+            # nodes.append(ListNode(val))
+            dummyNode.next = ListNode(val)
+            dummyNode = dummyNode.next
+            carr = value // 10 if value >= 10 else 0
+            temp1 = temp1.next
+            temp2 = temp2.next
+        
+        while temp1:
+            value = temp1.val + carr
+            # nodes.append(ListNode(value % 10))
+            dummyNode.next = ListNode(value % 10)
+            dummyNode = dummyNode.next
+            carr = value // 10 if value >= 10 else 0
+            temp1 = temp1.next
+        while temp2:
+            value = temp2.val + carr
+            # nodes.append(ListNode(value % 10))
+            dummyNode.next = ListNode(value % 10)
+            dummyNode = dummyNode.next
+            carr = value // 10 if value >=10 else 0
+            temp2 = temp2.next
+        if carr:
+            dummyNode.next = ListNode(carr)
+            # nodes.append(ListNode(carr))
+        
+        # for node in nodes:
+        #     dummyNode.next = node
+        #     dummyNode = dummyNode.next
+        
+        return sol.next
